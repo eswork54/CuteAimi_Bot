918c71d6ebabfa5b1935f0a1c412aeab8197ba08
eswork54 <55404299+eswork54@users.noreply.github.com>
2020-05-17 22:59:43 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,27 +15,8 @@ bot = commands.Bot(command_prefix='[')
 async def on_ready():
     print(">> Bot is online <<")
 
-@bot.event
-async def on_member_join(member):
-    channel = bot.get_channel(int(jdata['Welcome_channel']))
-    await channel.send(f'{member} join!')
-
-@bot.event
-async def on_member_remove(member):
-    channel = bot.get_channel(int(jdata['Leave_channel']))
-    await channel.send(f'{member} leave!')
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f'{round(bot.latency*1000)}(ms)')
-
 @bot.command()
 async def 吃啥(ctx):
-    user_id = ctx.author.id
-    await ctx.send(f'<@{user_id}>')
-
-@bot.command()
-async def 看看(ctx):
     food_dict = edata
     all_data = []
     for v,w in list(food_dict.items()):
@@ -45,11 +26,8 @@ async def 看看(ctx):
         all_data.extend(temp)
     random.shuffle(all_data)#打亂數據
     you_eat = random.choice(all_data)
-
-    await ctx.send(you_eat) 
-
-
+    user_id = ctx.author.id
+    await ctx.send(f'<@{user_id}> 去吃{you_eat}') 
 
 
 bot.run(jdata['TOKEN'])
-
